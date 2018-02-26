3086cb561d94b991212336a1bc278caf97fb4c0d
dongchris <cadong@usfca.edu>
2018-02-25 19:29:18 -0800

    editush

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, abort, request 
 from flask import render_template # finds and renders files under */templates/
-# from deploy_sprint2 import prefix
-prefix = 'cccc'
+from deploy_sprint2 import prefix
+
 # Initialization 
 # Create an application instance (an object of class Flask)  which handles all requests.
 application = Flask(__name__)
--- deploy_sprint2.py
+++ deploy_sprint2.py
@@ -21,6 +21,6 @@ def deploy(path_to_ssh_key_private_key, server_address, prefix):
 
 path_to_ssh_key_private_key = '/home/chris/cadong/1BigData/haydaysprint1/sprint_hayday.pem'
 server_address = 'ec2-34-217-148-56.us-west-2.compute.amazonaws.com'
-prefix = 'ddd'
+prefix = 'zzz'
 
 deploy(path_to_ssh_key_private_key, server_address, prefix)
