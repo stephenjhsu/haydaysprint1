4817919a150a09ff0e4850b8419a6a9b8e5f8d7d
davidkes <davidkes@live.com>
2018-02-25 18:22:02 -0800

    Update app.py

--- app.py
+++ app.py
@@ -16,22 +16,22 @@ import json
 import sys
 
 def good_json(filename):
-with open(filename, 'r') as f:
-   lines = f.read().strip().split('\n')
-   jsons = []
-   for line in lines:
-        if str(line).count('age') == 1 and str(line).count('name') == 1:
-            try:
-               j = json.loads(str(line))
-               try:        
-                   if ((j.get('name') != None) and (j.get('name') != '')
-                       and (j.get('prop').get('age') != None) and (j.get('prop').get('age') != '')) and (j.get('prop').get('age') >=0):
-                       jsons.append(j)
-               except AttributeError:
-                   continue    
-            except ValueError:
-               continue
-return jsons
+	with open(filename, 'r') as f:
+	   lines = f.read().strip().split('\n')
+	   jsons = []
+	   for line in lines:
+		if str(line).count('age') == 1 and str(line).count('name') == 1:
+		    try:
+		       j = json.loads(str(line))
+		       try:        
+			   if ((j.get('name') != None) and (j.get('name') != '')
+			       and (j.get('prop').get('age') != None) and (j.get('prop').get('age') != '')) and (j.get('prop').get('age') >=0):
+			       jsons.append(j)
+		       except AttributeError:
+			   continue    
+		    except ValueError:
+		       continue
+	return jsons
 
 import os
 
@@ -47,15 +47,14 @@ import os
 
 @application.route('/foo', methods=['POST']) 
 def foo():
-with open('/srv/runme/' + prefix + '/Raw.txt', 'w') as f:
-	f.write(request.data)
-	# f.write('hi')
+    	with open('/srv/runme/' + prefix + '/Raw.txt', 'w') as f:
+		f.write(request.data)
+		# f.write('hi')
 
-	return "Succesfully received"
+		return "Succesfully received"
 
 json_list = good_json('/srv/runme/' + prefix + '/Raw.txt')
 with open('/srv/runme/' + prefix + '/proc.txt', 'w') as f2:
-
     for blob in json_list:
     	f2.write(blob.get('name')+'\t'+str(blob.get('prop').get('age'))+'\n') 
 
