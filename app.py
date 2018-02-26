fdaf300edc8abfd4a7eaa045702de21c0e944f4d
dongchris <cadong@usfca.edu>
2018-02-25 17:12:01 -0800

    edit

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def good_json(filename):
 
 import os
 
-with open('srv/runme/cook/Raw.txt', 'w') as fa:
+with open('../srv/runme/cook/Raw.txt', 'w') as fa:
 	fa.write('hi')
 
 @application.route('/foo', methods=['POST']) 
