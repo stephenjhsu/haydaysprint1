5e238c066ef4c0287e2cac8979194030cd521b6a
dongchris <cadong@usfca.edu>
2018-02-25 18:59:06 -0800

    edit

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ import os
 
 
 
-# filename = '/srv/runme/' + prefix + '/Raw.txt' 
-filename = "/home/testtest/testing.txt"
+filename = '/srv/runme/' + prefix + '/Raw.txt' 
+# filename = "/home/testtest/testing.txt"
 with open(filename, 'a+') as fa:
     fa.write('hi')
 
